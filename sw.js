a82c28584dc153f50960290f8b1785a8ee7e4ddb
hanyuefei0409-pixel <hanyuefei0409@gmail.com>
2026-08-02 12:02:04 +0800

    改用站内 PDF.js，适配国内网络

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const SHELL='cet-study-shell-v6';
-const RUNTIME='cet-study-runtime-v6';
-const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./vocab-data.js','./exam-data.js','./data/questions.js','./data/extra_questions.js','./config.js'];
+const SHELL='cet-study-shell-v7';
+const RUNTIME='cet-study-runtime-v7';
+const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./vocab-data.js','./exam-data.js','./data/questions.js','./data/extra_questions.js','./config.js','./vendor/pdf.min.js','./vendor/pdf.worker.min.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(SHELL).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>![SHELL,RUNTIME,'cet-study-cet4-resources-v1'].includes(k)).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
