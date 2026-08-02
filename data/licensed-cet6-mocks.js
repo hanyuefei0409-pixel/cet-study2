fbcd5ddb05eef9afbcb9294772061d01496dcc00
hanyuefei0409-pixel <hanyuefei0409@gmail.com>
2026-08-02 15:33:20 +0800

    加入十套六级模拟题索引

---
+++ data/licensed-cet6-mocks.js
@@ -0,0 +1,7 @@
+window.CET_LICENSED_CET6_MOCKS=Array.from({length:10},(_,i)=>({
+  id:`licensed-cet6-${i+1}`,
+  title:`新东方六级模拟第${i+1}套`,
+  description:'六级模拟试卷与配套答案解析',
+  paper:`resources/cet6-licensed-mocks/mock-${i+1}-paper.pdf`,
+  answer:`resources/cet6-licensed-mocks/mock-${i+1}-answer.pdf`
+}));
