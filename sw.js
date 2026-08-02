a07ed89920566d4b557417957e6ec2608970c85d
hanyuefei0409-pixel <hanyuefei0409@gmail.com>
2026-08-02 11:51:21 +0800

    加入站内 PDF 阅读器，避免跳出应用

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SHELL='cet-study-shell-v5';
-const RUNTIME='cet-study-runtime-v5';
+const SHELL='cet-study-shell-v6';
+const RUNTIME='cet-study-runtime-v6';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./vocab-data.js','./exam-data.js','./data/questions.js','./data/extra_questions.js','./config.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(SHELL).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>![SHELL,RUNTIME,'cet-study-cet4-resources-v1'].includes(k)).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
