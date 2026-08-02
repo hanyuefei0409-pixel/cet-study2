6c147a147653226857e10fd591eb213b67c4dc49
hanyuefei0409-pixel <hanyuefei0409@gmail.com>
2026-08-02 15:32:53 +0800

    修复错题分类与每日计划自动重置

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const SHELL='cet-study-shell-v18';
-const RUNTIME='cet-study-runtime-v18';
-const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./vocab-data.js','./exam-data.js','./cet6-exam-data.js','./data/questions.js','./data/extra_questions.js','./data/practice-data.js','./data/full-mock-data.js','./data/licensed-cet4-mocks.js','./config.js','./vendor/pdf.min.js','./vendor/pdf.worker.min.js'];
+const SHELL='cet-study-shell-v19';
+const RUNTIME='cet-study-runtime-v19';
+const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./vocab-data.js','./exam-data.js','./cet6-exam-data.js','./data/questions.js','./data/extra_questions.js','./data/practice-data.js','./data/full-mock-data.js','./data/licensed-cet4-mocks.js','./data/licensed-cet6-mocks.js','./config.js','./vendor/pdf.min.js','./vendor/pdf.worker.min.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(SHELL).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>![SHELL,RUNTIME,'cet-study-cet4-resources-v1','cet-study-cet4-resources-v2','cet-study-cet6-resources-v2'].includes(k)).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
